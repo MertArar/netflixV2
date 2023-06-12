7743e1549c83d44d81ae582479dcdd2fa3c28de7
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-12 06:03:28 +0300

    bug fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import "./style.scss";
+
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
 import Popular from "./popular/Popular";
-import TopRated from "./topRated/topRated";
+import TopRated from "./topRated/TopRated";
 
 const Home = () => {
   return (
