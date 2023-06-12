9396a1f371b6bc2ab2c22b482e87a65e684267f8
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-12 06:17:35 +0300

    All home page done

--- src/pages/home/topRated/topRated.jsx
+++ src/pages/home/topRated/topRated.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-const topRated = () => {
-  return <div>topRated</div>;
+import Carousel from "../../../components/carousel/Carousel";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+
+import useFetch from "../../../hooks/useFetch";
+
+const TopRated = () => {
+  const [endpoint, setEndpoint] = useState("movie");
+
+  const { data, loading } = useFetch(`/${endpoint}/top_rated`);
+
+  const onTabChange = (tab) => {
+    setEndpoint(tab === "Movies" ? "movie" : "tv");
+  };
+
+  return (
+    <div className="carouselSection">
+      <ContentWrapper>
+        <span className="carouselTitle">Top Rated</span>
+        <SwitchTabs data={["Movies", "TV Shows"]} onTabChange={onTabChange} />
+      </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} endpoint={endpoint} />
+    </div>
+  );
 };
 
-export default topRated;
+export default TopRated;
