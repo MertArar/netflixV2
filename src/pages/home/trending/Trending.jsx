adc40596d188f146a0586f83d9cefc35ed79fe9a
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-12 05:54:20 +0300

    genres added

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -7,12 +7,12 @@ import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 import useFetch from "../../../hooks/useFetch";
 
 const Trending = () => {
-  const [endpoint, setEndPoint] = useState("day");
+  const [endpoint, setEndpoint] = useState("day");
 
   const { data, loading } = useFetch(`/trending/movie/${endpoint}`);
 
   const onTabChange = (tab) => {
-    setEndPoint(tab === "Day" ? "day" : "week");
+    setEndpoint(tab === "Day" ? "day" : "week");
   };
 
   return (
