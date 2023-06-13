387094276948219cd69a1e00aa08ebf5e3b93441
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-13 05:00:35 +0300

    footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 
+import { FaGithub, FaLinkedin, FaMedium, FaUser } from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+          veritatis libero, magni, ipsam culpa autem dolorem distinctio possimus
+          laborum odio accusantium quod ullam voluptatibus quis. Magnam corporis
+          maiores distinctio quos dolores deserunt, fugit sed nostrum aut
+          repellendus cumque inventore eaque natus tenetur. Impedit voluptas
+          error modi veritatis, cumque inventore at.
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaGithub />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+          <span className="icon">
+            <FaMedium />
+          </span>
+          <span className="icon">
+            <FaUser />
+          </span>
+        </div>
+        <div className="infoText2">
+          Copyright © 2023 Mert Arar. All rights reserved.
+        </div>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default Footer;
