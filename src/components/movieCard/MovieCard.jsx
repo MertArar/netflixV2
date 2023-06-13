222f299ff6736df4372ee96d4a99a4729bc3a344
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-13 05:34:29 +0300

    movieCard added

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import "./style.scss";
-
 import Img from "../lazyLoadImage/Img";
 import CircleRating from "../circleRating/CircleRating";
 import Genres from "../genres/Genres";
